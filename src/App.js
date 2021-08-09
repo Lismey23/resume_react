98f1a31c5098456b34e6f3395fc53c10a5c327e6
Lismey23 <74835876+Lismey23@users.noreply.github.com>
2021-08-08 20:17:04 -0400

    update

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App (){
                 <NavBar/>
                 <Switch>
                 <Route exact path='/' component={Home}/>
-                <Route exact path='/portfolio' component={Portfolio}/>
+                <Route exact path='/portfolio1' component={Portfolio}/>
 
                 </Switch>
 
